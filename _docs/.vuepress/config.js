3ff5047648fbb1cab77669232cfbce5d2eb68acc
xiadd <xiadd0102@gmail.com>
2018-05-18 13:18:40 +0800

    add docs

---
+++ _docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dest: 'docs',
+  title: '数据接口聚合',
+  description: '数据接口大整理',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '小说', link: '/novel/' },
+      { text: '煎蛋', link: '/jandan/' },
+      { text: '快递', link: '/kuaidi/' },
+    ]
+  }
+}
---
+++ _docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+actionText: 小说 →
+actionLink: /novel/
+---
